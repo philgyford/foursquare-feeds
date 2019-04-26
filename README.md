a999170b4d206c4571b17951990d6f8c9376d895
Phil Gyford <phil@gyford.com>
2019-04-26 17:53:53 +0100

    First working version

--- README.md
+++ README.md
@@ -1,2 +1,93 @@
 # Foursquare Feeds
 
+A python script that will generate a iCal (`.ics`) feed of your recent checkins on [Foursquare][4sq]/[Swarm][swarm].
+
+Foursquare [used to have such feeds][feeds] but they've stopped working for me.
+
+**NOTE: This is new and untested!**
+
+[4sq]: https://foursquare.com
+[swarm]: https://www.swarmapp.com
+[feeds]: https://foursquare.com/feeds/
+
+
+## Installation
+
+
+### 1. Make a Foursquare app
+
+Go to https://foursquare.com/developers/apps and create a new App.
+
+
+### 2. Install python requirements
+
+Either:
+
+    $ pipenv install
+
+or:
+
+    $ pip install -r requirements.txt
+
+
+### 3. Set up config file
+
+Copy `config_example.ini` to `config.ini`. Change the `IcsFilepath` to wherever you want your file to be saved.
+
+To get the `AccessToken` for your Foursquare app, you will have to go through the laborious procedure in step 4...
+
+
+### 4. Get an access token
+
+On https://foursquare.com/developers/apps, in your app, set the Redirect URI to `http://localhost:8000/`
+
+In a terminal window, open a python shell:
+
+    $ python
+
+and, using your app's Client ID and Client Secret enter this:
+
+```python
+import foursquare
+client = foursquare.Foursquare(client_id='YOUR_CLIENT_ID' client_secret='YOUR_CLIENT_SECRET', redirect_uri='http://localhost:8000')
+client.oauth.auth_url()
+```
+
+This will output something like:
+
+    'https://foursquare.com/oauth2/authenticate?client_id=YOUR_CLIENT_ID&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8000%2F'
+
+Copy the URL from your terminal *without the surrounding quotes* and paste it into a web browser.
+
+Your browser should redirect to a URL like the one below, with an error about not being able to connect to the server (unless you have a webserver running locally on your machine):
+
+    http://localhost:8000/?code=XX_CODE_RETURNED_IN_REDIRECT_XX#_=_
+
+Copy the code represented by `XX_CODE_RETURNED_IN_REDIRECT_XX` (note that there may be an extra `#_=_` on the end which *you should not copy*).
+
+Back in your python shell, with that code:
+
+```python
+client.oauth.get_token('XX_CODE_RETURNED_IN_REDIRECT_XX')
+```
+
+This will output another long code, which is your Access Token.
+
+Enter this in your `config.ini`.
+
+### 5. Run it
+
+Then run the script:
+
+    $ ./generate_feeds.py
+
+It should create an `.ics` file.
+
+If the file is generated in a location on your website that's publicly-readable, you should be able to subscribe to it from a calendar application. Then run the script periodically to have it update.
+
+Note that the file might contain private checkins or information you don't want to be public. In which case, probably best to make the name of any such publicly-readable file very obscure.
+
+
+## TODO
+
+* Upgrade `ics` when there's a release newer than 0.4. We currently use a specific commit because it's newer than 0.4 and uses a newer version of arrow, which we need for timezone shifts.
--- generate_feeds.py
+++ generate_feeds.py
@@ -1,17 +1,29 @@
+#!/usr/bin/env python3
 import configparser
 import logging
+
+import arrow
+import foursquare
+from ics import Calendar, Event
 
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 logger = logging.getLogger(__name__)
 
 CONFIG_FILE = "config.ini"
 
+# How many to fetch and use. Up to 250.
+NUM_CHECKINS = 100
+
 
 class FeedGenerator:
     def __init__(self):
+        "Loads config, sets up Foursquare API client."
         self._load_config(CONFIG_FILE)
 
+        self.client = foursquare.Foursquare(access_token=self.api_access_token)
+
     def _load_config(self, config_file):
+        "Set object variables based on supplied config file."
         config = configparser.ConfigParser()
 
         try:
@@ -20,15 +32,111 @@ class FeedGenerator:
             logger.critical("Can't read config file: " + config_file)
             exit()
 
-        self.api_id = config.get("Foursquare", "ClientID")
-        self.api_secret = config.get("Foursquare", "ClientSecret")
+        self.api_access_token = config.get("Foursquare", "AccessToken")
+        self.ics_filepath = config.get("Local", "IcsFilepath")
 
     def generate(self):
-        logger.info("GENERATE")
+        ""
+        checkins = self._get_checkins()
+
+        calendar = self._generate_calendar(checkins)
+
+        with open(self.ics_filepath, "w") as f:
+            f.writelines(calendar)
+
+        exit(0)
+
+    def _get_checkins(self):
+        "Returns a list of recent checkins for the authenticated user."
+
+        try:
+            return self.client.users.checkins(
+                params={"limit": NUM_CHECKINS, "sort": "newestfirst"}
+            )
+        except foursquare.FoursquareException as e:
+            logger.error("Error getting checkins: {}".format(e))
+            exit(1)
+
+    def _get_user_url(self):
+        "Returns the Foursquare URL for the authenticated user."
+        try:
+            user = self.client.users()
+        except foursquare.FoursquareException as e:
+            logger.error("Error getting user: {}".format(e))
+            exit(1)
+
+        return user["user"]["canonicalUrl"]
+
+    def _generate_calendar(self, checkins):
+        """Supplied with a list of checkin data from the API, generates an
+        ics Calendar object and returns it.
+        """
+        user_url = self._get_user_url()
+
+        c = Calendar()
+
+        for checkin in checkins["checkins"]["items"]:
+            venue_name = checkin["venue"]["name"]
+            tz_offset = self._get_checkin_timezone(checkin)
+
+            e = Event()
+
+            e.name = "@ {}".format(venue_name)
+            e.location = venue_name
+            e.url = "{}/checkin/{}".format(user_url, checkin["id"])
+            e.uid = "{}@foursquare.com".format(checkin["id"])
+            e.begin = checkin["createdAt"]
+
+            # Use the 'shout', if any, and the timezone offset in the description.
+            description = []
+            if "shout" in checkin and len(checkin["shout"]) > 0:
+                description = [checkin["shout"]]
+            description.append("Timezone offset: {}".format(tz_offset))
+            e.description = "\n".join(description)
+
+            # Use the venue_name and the address, if any, for the location.
+            location = venue_name
+            if "location" in checkin["venue"]:
+                loc = checkin["venue"]["location"]
+                if "formattedAddress" in loc and len(loc["formattedAddress"]) > 0:
+                    address = ", ".join(loc["formattedAddress"])
+                    location = "{}, {}".format(location, address)
+            e.location = location
+
+            c.events.add(e)
+
+        return c
+
+    def _get_checkin_timezone(self, checkin):
+        """Given a checkin from the API, returns a string representing the
+        timezone offset of that checkin.
+        In the API they're given as a number of minutes, positive or negative.
+
+        e.g. if offset is 60,   this returns '+01:00'
+             if offset is 0,    this returns '+00:00'
+             if offset is -480, this returns '-08:00'
+        """
+        # In minutes, e.g. 60 or -480
+        minutes = checkin["timeZoneOffset"]
+
+        # e.g. 1 or -8
+        hours = minutes / 60
+
+        # e.g. '01.00' or '-08.00'
+        if hours >= 0:
+            offset = "{:05.2f}".format(hours)
+            symbol = "+"
+        else:
+            offset = "{:06.2f}".format(hours)
+            symbol = ""
+
+        # e.g. '+01:00' or '-08.00'
+        return "{}{}".format(symbol, offset).replace(".", ":")
 
 
 if __name__ == "__main__":
 
     generator = FeedGenerator()
-
     generator.generate()
+
+    exit(0)
